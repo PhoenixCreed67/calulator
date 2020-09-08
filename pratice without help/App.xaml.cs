1ea94eb29536fa7c19ee57dc7b2642fce90a2731
PhoenixCreed67 <70340881+PhoenixCreed67@users.noreply.github.com>
2020-09-08 12:14:21 -0500

    Add files via upload

---
+++ pratice without help/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace pratice_without_help
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ pratice without help/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+
+namespace pratice_without_help
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void NumberPlace1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            double number1 = 0, number2 = 0, total = 0;
+
+            number1 = Convert.ToDouble(textBox1.Text);
+
+            number2 = Convert.ToDouble(textBox2.Text);
+
+            total = number1 + number2;
+
+            label.Content = total;
+
+
+
+
+
+
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            double number1 = 0, number2 = 0, total = 0;
+
+            number1 = Convert.ToDouble(textBox1.Text);
+
+            number2 = Convert.ToDouble(textBox2.Text);
+
+            total = number1 - number2;
+
+            label.Content = total;
+
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            double number1 = 0, number2 = 0, total = 0;
+
+            number1 = Convert.ToDouble(textBox1.Text);
+
+            number2 = Convert.ToDouble(textBox2.Text);
+
+            total = number1 * number2;
+
+            label.Content = total;
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            double number1 = 0, number2 = 0, total = 0;
+
+            number1 = Convert.ToDouble(textBox1.Text);
+
+            number2 = Convert.ToDouble(textBox2.Text);
+
+            total = number1 / number2;
+
+            label.Content = total;
+
+        }
+    }
+
+}
